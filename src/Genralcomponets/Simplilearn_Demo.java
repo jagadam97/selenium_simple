d7037718325928fbf05f95ff3447b1b19c2bd816
Jagadam Dinesh Reddy <dineshjagadam@gmail.com>
2022-07-05 09:32:21 +0530

    first commit

---
+++ src/Genralcomponets/Simplilearn_Demo.java
@@ -0,0 +1,105 @@
+package Genralcomponets;
+
+import java.util.Iterator;
+import java.util.Set;
+import org.openqa.selenium.*;
+
+public class Simplilearn_Demo extends basic_setup {
+	String er_nw = "Online Selenium Certification | Selenium Training | Selenium Course";
+	String er_pw = "Search Simplilearn Online Courses - #1 Certification Bootcamp";
+	String ar_nw = null;
+	String ar_pw = null;
+	public void openweb()
+	{
+		driver.get(Simplilearn);
+		driver.manage().window().maximize();
+	}
+	
+	public void search() throws InterruptedException
+	{
+		driver.findElement(By.id("header_srch")).sendKeys("Selenium");
+		
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//span[@class='search_icon input-search-icon']")).click();
+		Thread.sleep(2000);
+		try
+		{
+		driver.findElement(By.xpath("//h2[contains(text(),'Selenium 3.0 Training')]")).click();
+		}
+		catch(Exception e)
+		{
+			System.out.println("failed to load webpage fast enough");
+		}
+		Thread.sleep(1000);
+		String parentWindowHandler = driver.getWindowHandle();
+		String subWindowHandler = null;
+
+		Set<String> handles = driver.getWindowHandles(); 
+		Iterator<String> iterator = handles.iterator();
+		while (iterator.hasNext()){
+		    subWindowHandler = iterator.next();
+		}
+		
+		driver.switchTo().window(subWindowHandler);
+
+		System.out.println("New window title: " + driver.getTitle());
+		ar_nw = driver.getTitle();
+		Thread.sleep(300);
+
+		driver.switchTo().window(parentWindowHandler);
+		System.out.println("Parent window title : " + driver.getTitle());
+		ar_pw = driver.getTitle();
+		driver.get("https://google.com");
+		System.out.println("Parent window title : " + driver.getTitle());
+		driver.quit();
+	}
+
+	public void test()
+	{
+		
+	if(er_nw.equalsIgnoreCase(ar_nw))
+		{
+			System.out.println(ANSI_GREEN + "Validated New Window" + ANSI_RESET);
+		}
+	else
+		{
+		System.out.println(ANSI_YELLOW + "Failed validating New Window" + ANSI_RESET);
+		}
+		
+		
+		
+	if(er_pw.equalsIgnoreCase(ar_pw))
+		{
+			System.out.println(ANSI_GREEN + "Validated Parent Window" + ANSI_RESET);
+		}
+	
+	else
+		{
+			System.out.println(ANSI_YELLOW + "Failed validating New Window" + ANSI_RESET);
+		}
+	
+	
+	}
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		Simplilearn_Demo d = new Simplilearn_Demo();
+		d.Firefox();
+		d.openweb();
+		d.search();
+		d.test();
+		
+		d.Edge();
+		d.openweb();
+		d.search();
+		d.test();
+		
+		d.Chrome();
+		d.openweb();
+		d.search();
+		d.test();
+		
+	}
+
+}
---
+++ src/Genralcomponets/basic_setup.java
@@ -0,0 +1,44 @@
+package Genralcomponets;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class basic_setup
+{
+	String Simplilearn = "https://www.simplilearn.com/";
+	WebDriver driver;
+	//Drivers set Property and Switching Drivers
+	public void Firefox()
+	{
+		System.setProperty("webdriver.gecko.driver","C:\\Learn selinium\\Drivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		System.out.println(ANSI_CYAN + "Driver is set to Gecko Driver(Working in Firefox)" + ANSI_RESET );
+	}
+	public void Edge()
+	{
+		System.setProperty("webdriver.edge.driver", "C:\\Learn selinium\\Drivers\\msedgedriver.exe");
+		driver = new EdgeDriver();
+		System.out.println(ANSI_CYAN + "Driver is set to MSEdge Driver(Working in Edge)" + ANSI_RESET );
+	}
+	public void Chrome()
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Learn selinium\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		System.out.println(ANSI_CYAN + "Driver is set to Chrome Driver(Working in Chrome)" + ANSI_RESET );
+	}
+	// colors for prints
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+	
+}
